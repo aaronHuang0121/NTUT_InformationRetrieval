06d18aad2b660c613db2058334344c055f3f0c1e
tang03130313 <49488886+tang03130313@users.noreply.github.com>
2020-10-11 18:18:02 +0800

    Add files via upload

--- hw1.py
+++ hw1.py
@@ -8,49 +8,46 @@ from nltk.corpus import stopwords
 nltk.download('stopwords') 
 nltk.download('punkt')
 
-documents = []
+
 tokens_without_sw = []
+output = {} 
 punc = '''!()-[]{};:'"\, <>./?@#$%^&*_~\n'''
 
 f = warc.open("01.warc.gz")
-count = 0
+count = 1
 
 for record in f:
     if record.type == 'response':
         print(record.type, " " , record.url)
         r = requests.get(record.url)
         read = r.text.lower()
-        documents.append(read)
         text_tokens = word_tokenize(read) 
         tmp = [ word for word in text_tokens if not word in stopwords.words()]
         tmp = sorted(set(tmp),key=tmp.index)
-        tokens_without_sw.append(tmp)
+        tokens_without_sw = tmp
+        check = read.split()
+        print(check)
+        for item in tokens_without_sw:
+            print(str(item))
+            if str(item) not in output:
+                output[str(item)] = {}
+                output[str(item)][' total'] = check.count(str(item))
+                output[str(item)][count] = []
+                for j in range(1,len(check)+1):
+                    if check[j] == item:
+                        output[str(item)][count].append(j)
+                print(output[item])
+            else:
+                output[str(item)][' total'] += check.count(str(item))
+                if count not in output[str(item)]:
+                    output[str(item)][count] = []
+                for j in range(1,len(check)+1):
+                    if check[j] == item:
+                        output[str(item)][count].append(j)
+                print(output[item])
+
         count += 1
         if count == 2:
             break
-    
-output = {} 
-for i in range(0,len(documents)):
-    check = documents[i].lower().split()
-    print(check)
-    for item in tokens_without_sw[i]:
-        print(str(item))
-        if str(item) not in output:
-            output[str(item)] = {}
-            output[str(item)][' total'] = check.count(str(item))
-            output[str(item)][i+1] = []
-            for j in range(0,len(check)):
-                if check[j] == item:
-                    output[str(item)][i+1].append(j+1)
-            print(output[item])
-        else:
-            output[str(item)][' total'] += check.count(str(item))
-            if i+1 not in output[str(item)]:
-                output[str(item)][i+1] = []
-            for j in range(0,len(check)):
-                if check[j] == item:
-                    output[str(item)][i+1].append(j+1)
-            print(output[item])
-
 
 print(output) 
